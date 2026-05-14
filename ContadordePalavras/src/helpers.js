6b80d385b33573096780937e458681ab0901ee77
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2026-05-14 14:08:19 -0300

    Improves paragraph and output filtering logic

--- ContadordePalavras/src/helpers.js
+++ ContadordePalavras/src/helpers.js
@@ -1,14 +1,15 @@
-function filtraOcorrencia(paragrafo){
-   return Object.keys(paragrafo).filter(chave => paragrafo[chave] >1)
+function filtraOcorrencia(paragrafo) {
+  return Object.keys(paragrafo).filter((chave) => paragrafo[chave] > 1);
 }
 
-function montaSaidaArquivo(listaPalavras){
-    let textoFinal = ''
-    listaPalavras.forEach((paragrafo, indice) => {
-        const duplicadas = filtraOcorrencia(paragrafo).join(', ')
-        textoFinal += `Palavras duplicadas do paragrafo ${indice +1}: ${duplicadas} \n`
-    })
-    return textoFinal
+function montaSaidaArquivo(listaPalavras) {
+  let textoFinal = "";
+  listaPalavras.forEach((paragrafo, indice) => {
+    const duplicadas = filtraOcorrencia(paragrafo).join(", ");
+    if (!duplicadas) return;
+    textoFinal += `Palavras duplicadas do paragrafo ${indice + 1}: ${duplicadas}\n`;
+  });
+  return textoFinal;
 }
 
-export{montaSaidaArquivo}
+export { montaSaidaArquivo };
--- ContadordePalavras/src/index.js
+++ ContadordePalavras/src/index.js
@@ -1,27 +1,30 @@
-export function contaPalavras(texto){
-    const paragrafos = extraiParagrafos(texto)
-    const contagem = paragrafos.flatMap((paragrafo)=>{
-        if(!paragrafo) return []
-        return verificaPalavrasDuplicadas(paragrafo)
-    })
-    return contagem
+export function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto);
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  });
+  return contagem;
+}
+function extraiParagrafos(texto) {
+  return texto
+    .toLowerCase()
+    .split("\n")
+    .map((linha) => linha.trim())
+    .filter((linha) => linha.length > 0);
+}
 
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\'/#!$%\^&\*;:{}=\-_`~()]/g, "");
 }
-function extraiParagrafos(texto){
-    return texto.toLowerCase().split('\n')
-
-}
-function limpaPalavras (palavra){
-    return palavra.replace(/[.,\'/#!$%\^&\*;:{}=\-_`~()]/g, '')
-}
-function verificaPalavrasDuplicadas(texto){
-    const listaPalavras = texto.split(' ')
-    const resultado = {}
-    listaPalavras.forEach(palavra =>{
-        if (palavra.length >= 3 ){
-            const palavraLimpa = limpaPalavras(palavra)
-            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0)+1
-        }
-    })
-    return resultado
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  listaPalavras.forEach((palavra) => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+  return resultado;
 }
