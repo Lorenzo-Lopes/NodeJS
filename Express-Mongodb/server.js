b6678e886fadb826d2b25a24d3ed22d2f851e72b
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2026-05-14 16:56:00 -0300

    mudanças para uso do express e criação de rotas

--- Express-Mongodb/server.js
+++ Express-Mongodb/server.js
@@ -1,11 +1,7 @@
 import app from "./src/app.js"
 const PORT = 3000
 
-const rotas= {
-    "/":"Curso de Node.js",
-    "/livros": "Entrei na rota Livros",
-    "/autores": "Entrei na rota Autores"
-}
+
 
 app.listen(PORT, () =>{
     console.log('Servidor Escutando')
