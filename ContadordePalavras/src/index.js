6360752840dc73852b46d7ee0854688fd17a7216
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2026-05-14 09:39:18 -0300

    func quebrando paragrafos

--- ContadordePalavras/src/index.js
+++ ContadordePalavras/src/index.js
@@ -4,14 +4,29 @@ const caminho = process.argv
 const link = caminho[2]
 
 fs.readFile(link, 'utf-8',(erro,texto)=>{
-    verificaPalavrasDuplicadas(texto)
+    quebrandoParagrafos(texto)
+    //verificaPalavrasDuplicadas(texto)
 })
 
+function quebrandoParagrafos(texto){
+    const paragrafos = texto.toLowerCase().split('\n')
+    const contagem = paragrafos.flatMap((paragrafo)=>{
+        if(!paragrafo) return []
+        return verificaPalavrasDuplicadas(paragrafo)
+    })
+    console.log(contagem)
+}
+function limpaPalavras (palavra){
+    return palavra.replace(/[.,\'/#!$%\^&\*;:{}=\-_`~()]/g, '')
+}
 function verificaPalavrasDuplicadas(texto){
     const listaPalavras = texto.split(' ')
     const resultado = {}
     listaPalavras.forEach(palavra =>{
-        resultado[palavra] = (resultado[palavra] || 0)+1
+        if (palavra.length >= 3 ){
+            const palavraLimpa = limpaPalavras(palavra)
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0)+1
+        }
     })
-    console.log(resultado)
+    return resultado
 }
