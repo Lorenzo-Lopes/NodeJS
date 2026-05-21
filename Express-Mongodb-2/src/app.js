73d542fbd49617871ad2fb8d307534cd6033196f
lorenzo-lopes <lorenzolopes.dev@gmail.com>
2026-05-21 13:37:55 -0300

    Refactors error handling to use centralized middleware
    Routes all controller errors through Express error middleware, replacing inline error responses for consistency and maintainability.
    Adds specific handling for Mongoose CastError with user-friendly messaging.
    Improves code structure and prepares for scalable error management.

--- Express-Mongodb-2/src/app.js
+++ Express-Mongodb-2/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
+import mongoose from "mongoose";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -10,5 +11,16 @@ db.once("open", () => {
 const app = express();
 app.use(express.json());
 routes(app);
+
+// eslint-disable-next-line no-unused-vars
+app.use((erro, req, res, next) => {
+  if (erro instanceof mongoose.Error.CastError) {
+    return res
+      .status(400)
+      .send({ message: "Dados fornecidos estão incorretos.]" });
+  } else {
+    return res.status(500).send("Erro interno ");
+  }
+});
 
 export default app;
